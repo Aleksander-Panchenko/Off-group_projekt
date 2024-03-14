17c9e61ac51c5f13057040d61c4d5b717de122de
Aleksandr Panchenko <lordfreeze1986@gmail.com>
2024-03-14 10:48:25 +0300

    final version 1.0

--- JS/script.js
+++ JS/script.js
@@ -11,8 +11,60 @@ const initMan = document.querySelector(".header-init-link");
 const initText = document.querySelector(".header-init-text");
 const telega = document.querySelector(".header-social-telega");
 const wattsap = document.querySelector(".header-social-watsap");
+const telegaFooter = document.querySelector(".footer-social-link-telega");
+const wattsapFooter = document.querySelector(".footer-social-link-wattsap");
+const telegaFooterMini = document.querySelector(".footer-social-link-telega-mini");
+const wattsapFooterMini = document.querySelector(".footer-social-link-wattsap-mini");
+const menuBtn = document.querySelector(".menu-mini-spanblock");
+const menuXBtn = document.querySelector(".menu-btn-x")
+const menu = document.querySelector(".menu");
+const docBtn1 = document.querySelector(".content-down-top-btn1");
+const docBtn2 = document.querySelector(".content-down-top-btn2");
+const doc1 = document.querySelector(".content-down-info");
+const doc2 = document.querySelector(".content-down-docs");
+const slider = document.querySelector(".slider-list");
+const pagination = document.querySelectorAll(".pagination");
 
 let boxNumberStart = 1;
+
+let slideNumber = 0;
+const paginationArr = Array.from(pagination);
+
+function sliderMove (evt) {
+    const target = evt.currentTarget;
+    pagination.forEach(function(item) {
+        item.classList.remove("pag-active")
+    });
+    target.classList.add("pag-active");
+    slideNumber = paginationArr.indexOf(target);
+    slider.style = `margin-left: -${100*slideNumber}%`
+}
+
+pagination.forEach(n => {
+    n.addEventListener("click" , sliderMove)
+})
+
+function docsOpen () {
+    docBtn1.classList.remove("content-down-top-btn1-active");
+    docBtn2.classList.add("content-down-top-btn1-active");
+    doc1.style = "display: none;"
+    doc2.style = "display: block;"
+}
+
+function docsClose () {
+    docBtn2.classList.remove("content-down-top-btn1-active");
+    docBtn1.classList.add("content-down-top-btn1-active");
+    doc2.style = "display: none;"
+    doc1.style = "display: block;"
+}
+
+function menuOpen () {
+    menu.classList.add("menu-open");
+}
+
+function menuClose () {
+    menu.classList.remove("menu-open");
+}
 
 function qwickOrder () {
     boxNumber.forEach(boxN => {
@@ -39,19 +91,19 @@ function blueInitOff () {
 }
 
 function telegaOn () {
-    telega.classList.add("header-social-telega-active");
+    this.classList.add("header-social-telega-active");
 }
 
 function telegaOff () {
-    telega.classList.remove("header-social-telega-active");
+    this.classList.remove("header-social-telega-active");
 }
 
 function wattsapOn () {
-    wattsap.classList.add("header-social-watsap-active");
+    this.classList.add("header-social-watsap-active");
 }
 
 function wattsapOff () {
-    wattsap.classList.remove("header-social-watsap-active");
+    this.classList.remove("header-social-watsap-active");
 }
 
 function btnYOn () {
@@ -86,3 +138,15 @@ btnBox.addEventListener("mouseover" , btnYOn);
 btnBox.addEventListener("mouseout" ,  btnYOff);
 btnQwik.addEventListener("mouseover" , btnBOn);
 btnQwik.addEventListener("mouseout" ,  btnBOff);
+telegaFooter.addEventListener("mouseover" , telegaOn);
+telegaFooter.addEventListener("mouseout" ,  telegaOff);
+wattsapFooter.addEventListener("mouseover" , wattsapOn);
+wattsapFooter.addEventListener("mouseout" ,  wattsapOff);
+telegaFooterMini.addEventListener("mouseover" , telegaOn);
+telegaFooterMini.addEventListener("mouseout" ,  telegaOff);
+wattsapFooterMini.addEventListener("mouseover" , wattsapOn);
+wattsapFooterMini.addEventListener("mouseout" ,  wattsapOff);
+menuBtn.addEventListener("click" , menuOpen);
+menuXBtn.addEventListener("click" , menuClose);
+docBtn2.addEventListener("click" , docsOpen);
+docBtn1.addEventListener("click" , docsClose);
